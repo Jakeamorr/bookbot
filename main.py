9e2d26e5faea3295d1f85c653dd3f087d746e9f6
Jake Morris <jakeamorr@gmail.com>
2025-03-03 14:19:55 -0500

    add stats file with get_word_count and get_char_count functions

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from stats import get_word_count, get_char_count
+
 def get_book_text(fp):
-    with open(fp) as f:
+    with open(fp, mode='r', encoding="utf-8-sig") as f:
         text = f.read()
         return text
 
 def main():
     file_path = "books/frankenstein.txt"
     book_text = get_book_text(file_path)
-    print(book_text)
+    word_count = get_word_count(book_text)
+    print(f"{word_count} words found in the document")
+    char_dict = get_char_count(book_text)
+    print(char_dict)
+
 
 main()
