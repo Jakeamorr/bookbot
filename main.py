d55a3b32eeaae647484e1f1726573ab6eaddedd1
Jake Morris <jakeamorr@gmail.com>
2025-03-03 22:53:06 -0500

    pass book path via sys args, add print report function

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, print_report
+import sys
 
 def get_book_text(fp):
-    with open(fp, mode='r', encoding="utf-8-sig") as f:
+    with open(fp) as f:
         text = f.read()
         return text
 
 def main():
-    file_path = "books/frankenstein.txt"
+    args = sys.argv
+    file_path = ""
+    if len(args) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return
+    else:
+        file_path = args[1]
+
     book_text = get_book_text(file_path)
     word_count = get_word_count(book_text)
-    print(f"{word_count} words found in the document")
     char_dict = get_char_count(book_text)
-    print(char_dict)
-
+    print_report(char_dict, word_count, file_path)
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,26 @@ def get_char_count(str):
 
     return char_dict
 
+def sort_key(dict):
+    return dict["count"]
+
+def sort_char_dict(char_dict):
+    sorted_char_dicts = []
+    for (k, v) in char_dict.items():
+        if k.isalpha():
+            sorted_char_dicts.append({"char": k, "count": v})
+    sorted_char_dicts.sort(reverse=True, key=sort_key)
+    return sorted_char_dicts
+
+def print_report(char_dict, word_count, file_path):
+    sorted_dict = sort_char_dict(char_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_dict:
+        print(f"{dict['char']}: {dict['count']}")
+    print("============= END ===============")
+
+
